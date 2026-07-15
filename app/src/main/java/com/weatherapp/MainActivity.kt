a51c1c1479c544b32b10f62a02532295c79a34a9
Pinmarigor <igor.vitor.ms00@gmail.com>
2026-07-15 20:00:41 -0300

    Prática 3 – Parte 3

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -18,8 +18,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.weatherapp.ui.CityDialog
 import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.nav.BottomNavBar
 import com.weatherapp.ui.nav.BottomNavItem
@@ -35,7 +40,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val viewModel : MainViewModel by viewModels()
+            var showDialog by remember { mutableStateOf(false) }
+
             WeatherAppTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
+
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -60,7 +74,7 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController, items)
                     },
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(onClick = { showDialog = true }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
