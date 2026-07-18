7b8f11d92f23ca2d6c16ed655e0c3b34451837b3
Pinmarigor <igor.vitor.ms00@gmail.com>
2026-07-18 14:04:31 -0300

    Prática 11 – Parte 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,8 @@ dependencies {
     implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
